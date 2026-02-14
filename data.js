bbfd959c9e9bfea1910bb610ea6996b37b2af2c5
ogsiddharth <mauryaji0115@gmail.com>
2026-02-14 19:35:19 +0530

    Revise product data with new image links
    Updated product images and URLs in productsData.

--- data.js
+++ data.js
@@ -1,13 +1,18 @@
 const productsData = [
-    { id: 1, name: "Fortune Chakki Atta 5kg", price: 245, cat: "Grains", bestseller: true, img: "https://m.media-amazon.com/images/I/81v7Wn9X9mL._SL1500_.jpg" },
-    { id: 2, name: "India Gate Rice 1kg", price: 90, cat: "Grains", bestseller: true, img: "https://m.media-amazon.com/images/I/71Y87L2V8FL._SL1500_.jpg" },
-    { id: 3, name: "Arhar Dal (Premium) 1kg", price: 160, cat: "Grains", bestseller: true, img: "https://m.media-amazon.com/images/I/71Hscf4W+CL._SL1500_.jpg" },
-    { id: 4, name: "Maggi Noodles 12-pack", price: 168, cat: "Snacks", bestseller: true, img: "https://m.media-amazon.com/images/I/81096-7Yn8L._SL1500_.jpg" },
-    { id: 5, name: "Lay's Magic Masala", price: 20, cat: "Snacks", bestseller: true, img: "https://m.media-amazon.com/images/I/719hS+v3C4L._SL1500_.jpg" },
+    {
+        id: 1, name: "Fortune Chakki Atta 5kg",
+        price: 245, cat: "Grains",
+        bestseller: true,
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIji-Bk-cKOuQdIPFeZGrMIl3fWPM7nfmOUg&s"
+    },
+    { id: 2, name: "India Gate Rice 1kg", price: 90, cat: "Grains", bestseller: true, img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRNJoxUGVW2Lv4r5XcUJxZVJXUiNt5wHCRQg&s" },
+    { id: 3, name: "Arhar Dal (Premium) 1kg", price: 160, cat: "Grains", bestseller: true, img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmM5lEyztKTAtx-45IHnmS9dbJRVkBtBd_nw&s" },
+    { id: 4, name: "Maggi Noodles 12-pack", price: 168, cat: "Snacks", bestseller: true, img: "https://i.chaldn.com/_mpimage/nestle-maggi-2-minute-masala-instant-noodles-12-pack?src=https%3A%2F%2Feggyolk.chaldal.com%2Fapi%2FPicture%2FRaw%3FpictureId%3D153593&q=best&v=1" },
+    { id: 5, name: "Lay's Magic Masala", price: 20, cat: "Snacks", bestseller: true, img: "https://m.media-amazon.com/images/I/71kow7ToNcL.jpg" },
 
-    { id: 6, name: "Oreo Biscuits 120g", price: 30, cat: "Snacks", img: "https://m.media-amazon.com/images/I/61NfT-m2p5L._SL1000_.jpg" },
-    { id: 7, name: "Coca Cola 750ml", price: 45, cat: "Drinks", img: "https://m.media-amazon.com/images/I/51v8ny96SGL._SL1000_.jpg" },
-    { id: 8, name: "Dettol Soap (Buy 3 Get 1)", price: 155, cat: "Care", img: "https://m.media-amazon.com/images/I/61xK0jI-YxL._SL1000_.jpg" },
-    { id: 9, name: "Surf Excel Matic 1kg", price: 220, cat: "Cleaning", img: "https://m.media-amazon.com/images/I/61SOnG-61KL._SL1000_.jpg" },
-    { id: 10, name: "Colgate MaxFresh 150g", price: 98, cat: "Care", img: "https://m.media-amazon.com/images/I/61R-S8U5YWL._SL1000_.jpg" }
+    { id: 6, name: "Oreo Biscuits 120g", price: 30, cat: "Snacks", img: "https://www.bbassets.com/media/uploads/p/l/1201975_16-oreo-creme-biscuits-vanilla-120g-choco-120g-strawberry-120g.jpg" },
+    { id: 7, name: "Coca Cola 750ml", price: 45, cat: "Drinks", img: "https://cdn.dmart.in/images/products/MAY130001921xx13MAY25_8_B.jpg" },
+    { id: 8, name: "Dettol Soap (Buy 3 Get 1)", price: 155, cat: "Care", img: "https://www.jiomart.com/images/product/original/491653462/dettol-skincare-soap-with-moisturisers-75-g-buy-3-get-1-free-product-images-o491653462-p590124675-3-202203150542.jpg?im=Resize=(420,420)" },
+    { id: 9, name: "Surf Excel Matic 1kg", price: 220, cat: "Cleaning", img: "https://rukminim2.flixcart.com/image/704/844/xif0q/washing-powder/w/e/b/-original-imagyg5hyg5kyzxz.jpeg?q=90" },
+    { id: 10, name: "Colgate MaxFresh 150g", price: 98, cat: "Care", img: "https://m.media-amazon.com/images/I/81eketodExL.jpg" }
 ];
